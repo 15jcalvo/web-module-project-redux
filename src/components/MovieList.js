a3f4df47e3a80c82a430c15fffff55c7439f0210
James_Calvo <15jcalvo@gmail.com>
2021-12-08 19:47:50 -0500

    movies rendering from state with connect

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { connect } from 'react-redux'
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
+
+const mapStateToProps = (state)=> {
+    return({
+      movies: state.movies
+    })
+  }
 
 const MovieList = (props)=> {
     const movies = [];
@@ -21,7 +27,7 @@ const MovieList = (props)=> {
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        props.movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
                     }
                 </tbody>
             </table>
@@ -31,4 +37,4 @@ const MovieList = (props)=> {
     );
 }
 
-export default MovieList;
+export default connect(mapStateToProps) (MovieList);
