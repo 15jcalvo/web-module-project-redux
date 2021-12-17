37dfd2ea6363aba671975ed6dfd78ccf03928c16
James_Calvo <15jcalvo@gmail.com>
2021-12-16 19:20:31 -0500

    favorites reducer added not linked

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
+import favoritesReducer from './favoritesReducer';
+
+const reducer = combineReducers({movieReducer, favoritesReducer})
 
 export default movieReducer;
